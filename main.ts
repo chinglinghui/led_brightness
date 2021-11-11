da2df9f10756123b317c7afbcc5d5b62d1ced83b
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-11-11 16:47:13 +0800

    製作基礎陣列間隔亮燈

--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
+let list = [
+[
+0,
+1,
+0,
+1,
+0
+],
+[
+1,
+0,
+1,
+0,
+1
+],
+[
+0,
+1,
+0,
+1,
+0
+],
+[
+1,
+0,
+1,
+0,
+1
+],
+[
+0,
+1,
+0,
+1,
+0
+]
+]
+for (let index = 0; index <= 24; index++) {
+    if (list[1][index % 5 + 1] == 0) {
+        led.toggle(Math.floor(index / 5), index % 5)
+    }
+}
 basic.forever(function () {
 	
 })
