a9c1ee429df22aa930bf4a02c8ca8dd237f1cffc
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-11-18 16:58:29 +0800

    多功能鍵

--- main.ts
+++ main.ts
@@ -1,60 +1,198 @@
-input.onButtonPressed(Button.A, function () {
-    for (let Y = 0; Y <= 4; Y++) {
-        for (let X = 0; X <= 4; X++) {
-            led.plotBrightness(X, Y, 51 * list[Y][X])
-        }
+function type1 () {
+    if (randint(0, 2) == 0) {
+        list = [
+        [
+        0,
+        1,
+        2,
+        3,
+        4
+        ],
+        [
+        1,
+        2,
+        3,
+        4,
+        5
+        ],
+        [
+        2,
+        3,
+        4,
+        5,
+        0
+        ],
+        [
+        3,
+        4,
+        5,
+        0,
+        1
+        ],
+        [
+        4,
+        5,
+        0,
+        1,
+        2
+        ]
+        ]
     }
-    list.unshift(list.pop())
-    basic.pause(100)
-})
-let list: number[][] = []
-list = [
-[
-0,
-2,
-0,
-2,
-0
-],
-[
-3,
-0,
-3,
-0,
-3
-],
-[
-0,
-4,
-0,
-4,
-0
-],
-[
-5,
-0,
-5,
-0,
-5
-],
-[
-0,
-4,
-0,
-4,
-0
-]
-]
-while (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B) || input.buttonIsPressed(Button.AB))) {
-    for (let Y = 0; Y <= 4; Y++) {
-        for (let X = 0; X <= 4; X++) {
-            led.plotBrightness(X, Y, 51 * list[Y][X])
-        }
-    }
-    list.unshift(list.pop())
-    basic.pause(100)
 }
-basic.clearScreen()
+function type2 () {
+    if (randint(0, 2) == 0) {
+        list = [
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ]
+        ]
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    _type = 1
+})
+function type3 () {
+    if (randint(0, 2) == 0) {
+        list = [
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ],
+        [
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5),
+        randint(0, 5)
+        ]
+        ]
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    _type = 3
+})
+input.onButtonPressed(Button.B, function () {
+    _type = 2
+})
+function type0 () {
+    list = [
+    [
+    0,
+    5,
+    0,
+    5,
+    0
+    ],
+    [
+    5,
+    0,
+    5,
+    0,
+    5
+    ],
+    [
+    0,
+    5,
+    0,
+    5,
+    0
+    ],
+    [
+    5,
+    0,
+    5,
+    0,
+    5
+    ],
+    [
+    0,
+    5,
+    0,
+    5,
+    0
+    ]
+    ]
+}
+let list: number[][] = []
+let _type = 0
+_type = 0
 basic.forever(function () {
-	
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            for (let Y = 0; Y <= 4; Y++) {
+                for (let X = 0; X <= 4; X++) {
+                    led.plotBrightness(X, Y, 51 * list[Y][X])
+                }
+            }
+            list.unshift(list.pop())
+            basic.pause(100)
+            if (_type == 0) {
+                type0()
+            } else if (_type == 1) {
+                type1()
+            } else if (_type == 2) {
+                type2()
+            } else {
+                if (_type == 3) {
+                	
+                }
+            }
+        }
+    }
 })
